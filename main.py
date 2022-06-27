80316b88eb963ab5615425b3f9a9e776484bdcef
naut <jrbarnachea@gmail.com>
2022-06-27 15:39:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import argparse
 import pyautogui
 
 
-def press_key(key: str) -> None:
-    """Press a key. Gran Turismo requires you hold a keypress for a small duration."""
+def press_key(key):
+    """press a key (str). Note: Gran Turismo requires you hold a keypress_key for a small duration."""
     
-    with pyautogui.hold(key):
-        time.sleep(0.2)
-    time.sleep(0.2)
+    duration = 0.2
+    hold_key(key, duration)
+    time.sleep(duration)
 
 
-def hold_key(key: str, duration: float) -> None:
-    """Hold a key for some duration."""
+def hold_key(key, duration):
+    """Holds a key (str) for some duration (int/float)."""
 
     with pyautogui.hold(key):
         time.sleep(duration)
@@ -25,47 +25,165 @@ def focus_window():
 
     x, y = pyautogui.size()
     center = x / 2, y / 2
+    delay = 1
     pyautogui.moveTo(center)
-    time.sleep(1)
+    time.sleep(delay)
     pyautogui.click()
-    time.sleep(1)
+    time.sleep(delay)
 
 
-def detect_screen(screen):
+def detect_on_screen(file_name):
+    """Returns True if inputted image is a screen element that has been detected. False otherwise."""
 
-    width, height = pyautogui.size()
-    halfwidth = int(width / 2)
-    halfheight = int(height / 2)
+    x, y = pyautogui.size()
+    half_x = int(x / 2)
+    half_y = int(y / 2)
 
-    CONFIDENCE = 0.5
-    GRAYSCALE = True
-    REGION = (halfwidth, 0, halfwidth, halfheight)
+    confidence = 0.5
+    grayscale = True
+    region = (half_x, 0, half_x, half_y) # left, top, width, height
 
-    filename = screen + ".png"
-    print(" [-] Analyzing " + filename + " screen.")
+    path = f"images/{file_name}"
+    print("Analyzing " + file_name + " screen.")
     detected_screen = pyautogui.locateOnScreen(
-        filename, region=REGION, grayscale=GRAYSCALE, confidence=CONFIDENCE
+        path, region=region, grayscale=grayscale, confidence=confidence
     )
-    if detected_screen is not None:
-        return True
+    
+    return detected_screen is not None
+
+def open_extra_menu():
+    """Helper function to opens extra menu item."""
+
+    press_key("enter")
+    press_key("enter")
+    time.sleep(3)
+    press_key("escape")
+    press_key("escape")
+    press_key("escape")
+    time.sleep(4)
+    print("Extra menu item has been opened.")
+
+
+def open_ticket():
+    """Helper function to open ticket."""
+
+    # open ticket
+    press_key("enter")
+    press_key("enter")
+    time.sleep(15)
+    # Accept ticket
+    press_key("enter")
+    print("Ticket has been accepted.")
+
+
+def get_tickets():
+
+    # start at Cafe
+    print("Entering Cafe.")
+    press_key("enter")
+    time.sleep(3)
+    
+    # navigate and select My Collection
+    press_key("left")
+    press_key("enter")
+    
+    # navigate and select Extra Menus
+    press_key("down")
+    press_key("right")
+    press_key("enter")
+    
+    # navigate to extra menu no. 1
+    press_key("up")
+
+    # open extra menu no. 1
+    print("Opening extra menu no. 1.")
+    open_extra_menu()
+
+    # navigate to extra menu no. 3
+    press_key("right")
+
+    # open extra menu no. 3
+    print("Opening extra menu no. 3.")
+    open_extra_menu()
+
+
+def open_tickets():
+    
+    # navigate back to map to open ticket
+    press_key("right")
+    press_key("enter")
+    time.sleep(4)
+    press_key("right")
+    press_key("right")
+    press_key("right")
+    press_key("enter")
+
+    # select and open 4-Star ticket
+    print("Opening 4-Star ticket.")
+    open_ticket()
+
+    # screen detection for 4-Star ticket
+    if detect_on_screen("car_text.png"):
+        print("Car screen detected.")
+        press_key("enter")
+        time.sleep(10)
+        press_key("enter")
+        time.sleep(4)
+        press_key("enter")
+        press_key("enter")
+    
+    elif detect_on_screen("credits_text.png"):
+        print("Credits screen detected.")
+        press_key("enter")
+        time.sleep(5)
+        press_key("enter")
+    
+    elif detect_on_screen("tuning_text.png"):
+        print("Tuning part screen detected.")
+    
+    elif detect_on_screen("invitation_text.png"):
+        print("Invitation screen detected.")
+    
     else:
-        return False
+        print("No screen has been detected. Exiting.")
+        exit()
+
+    # navigate to 6-Star ticket
+    press_key("right")
+
+    # select and open 6-Star ticket
+    print("Opening 6-Star ticket.")
+    open_ticket()
+
+    # navigate back to map
+    print("Heading back to map.")
+    press_key("escape")
+    press_key("escape")
+    time.sleep(3)
+
+    # place cursor on Menu to start over
+    press_key("left")  
 
 
 def execute_bot():
     """Main bot script."""
-    pass
+
+    focus_window()
+    get_tickets()
+    open_tickets()
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="gt7_bot by naut 2022"
     )
-    subparser = parser.add_subparsers(dest='command')
-
-    parser.add_argument("--version", "-v", action="version", version="%(prog)s v0.0.1")
+    parser.add_argument("--version", "-v", action="version", version="%(prog)s v0.0.2")
 
     args = parser.parse_args()
 
-    
-    execute_bot()
+    cycles = 0
+    while True:
+        execute_bot()
+        cycles += 1
+        print(f"{str(cycles)} cyles completed.")
