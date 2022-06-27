d30f06ffd25eb58d9bac8dee8fae7122f5215290
naut <jrbarnachea@gmail.com>
2022-06-27 12:04:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import argparse
 import pyautogui
 
 
+def execute_bot():
+    """Main bot script."""
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="gt7_bot by naut 2022"
@@ -12,3 +16,6 @@ if __name__ == "__main__":
     parser.add_argument("--version", "-v", action="version", version="%(prog)s v0.0.1")
 
     args = parser.parse_args()
+
+    
+    execute_bot()
