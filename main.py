f154fda4e2add586aba4e83aece929643195a11b
naut <jrbarnachea@gmail.com>
2022-06-27 18:29:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,21 @@ def press_key(key):
     duration = 0.2
     hold_key(key, duration)
     time.sleep(duration)
+    if args.debug:
+        print(f"{key} pressed.")
 
 
 def hold_key(key, duration):
     """Holds a key (str) for some duration (int/float)."""
-
+    
     with pyautogui.hold(key):
         time.sleep(duration)
+
+
+def delay(duration):
+    if args.debug:
+        print(f"delay for {duration} seconds.")
+    time.sleep(duration)
 
 
 def focus_window():
@@ -53,34 +61,71 @@ def detect_on_screen(file_name):
     
     return detected_screen is not None
 
-def open_extra_menu():
+
+def open_extra_menu_item():
     """Helper function to open the extra menu item."""
 
     press_key("enter")
     press_key("enter")
-    time.sleep(3)
+    delay(3)
     press_key("escape")
     print("Extra menu item has been opened.")
 
 
 def open_ticket():
-    """Helper function to open a ticket."""
+    """Helper function to open a ticket. Goes through an RNG roll."""
 
-    # open ticket
-    press_key("enter")
-    press_key("enter")
-    time.sleep(15)
-    # Accept ticket
-    press_key("enter")
-    print("Ticket has been accepted.")
+    press_key("enter")  # open_ticket
+    press_key("enter")  # confirm to start the RNG roll.
+    print("RNGing.")
+    delay(16)
+
+
+def accept_ticket():
+    """Helper function to accept a ticket using screen detection, but default 
+    to normal acceptance if it fails."""
+
+    if detect_on_screen("car_text.png"):
+        print("Car screen detected.")
+        press_key("enter")
+        delay(10)
+        press_key("enter")
+        delay(4)
+        press_key("enter")
+        press_key("enter")
+    
+    elif detect_on_screen("credits_text.png"):
+        print("Credits screen detected.")
+        press_key("enter")
+        delay(5)
+        press_key("enter")
+    
+    elif detect_on_screen("tuning_text.png"):
+        print("Tuning part screen detected.")
+    
+    elif detect_on_screen("invitation_text.png"):
+        print("Invitation screen detected.")
+    
+    else:
+        # use default ticket acceptance otherwise.
+        press_key("enter")
+        delay(10)
+        press_key("enter")
+        delay(4)
+        press_key("enter")
+        press_key("enter")
+
+    press_key("enter")  # accept the ticket
+    print("Gift accepted.")
 
 
 def get_tickets():
+    """Gets all of the free tickets and goes back to home."""
 
-    # start at Cafe
-    press_key("enter")
+    # start at home (Cafe)
     print("Entering Cafe.")
-    time.sleep(3)
+    press_key("enter")
+    delay(3)
     
     # navigate and select My Collection
     press_key("left")
@@ -96,27 +141,28 @@ def get_tickets():
 
     # open extra menu no. 1
     print("Opening extra menu no. 1.")
-    open_extra_menu()
+    open_extra_menu_item()
 
     # navigate to extra menu no. 3
     press_key("right")
 
     # open extra menu no. 3
     print("Opening extra menu no. 3.")
-    open_extra_menu()
+    open_extra_menu_item()
 
-    # navigate back to Cafe
+    # navigate back to home (Cafe)
     press_key("escape")
     press_key("escape")
-    time.sleep(4)
+    delay(4)
 
 
 def open_tickets():
+    """Opens all tickets."""
     
     # navigate to Garage
     press_key("right")
     press_key("enter")
-    time.sleep(4)
+    delay(4)
 
     # navigate to Gifts
     press_key("right")
@@ -124,51 +170,31 @@ def open_tickets():
     press_key("right")
     press_key("enter")
 
-    # select and open 4-Star ticket
-    print("Opening 4-Star ticket.")
+    # select and open 4-Star roulette ticket
+    print("Opening 4-Star roulette ticket.")
     open_ticket()
 
-    # screen detection for 4-Star ticket
-    if detect_on_screen("car_text.png"):
-        print("Car screen detected.")
-        press_key("enter")
-        time.sleep(10)
-        press_key("enter")
-        time.sleep(4)
-        press_key("enter")
-        press_key("enter")
-    
-    elif detect_on_screen("credits_text.png"):
-        print("Credits screen detected.")
-        press_key("enter")
-        time.sleep(5)
-        press_key("enter")
-    
-    elif detect_on_screen("tuning_text.png"):
-        print("Tuning part screen detected.")
-    
-    elif detect_on_screen("invitation_text.png"):
-        print("Invitation screen detected.")
-    
-    else:
-        print("No screen has been detected. Exiting.")
-        exit()
+    # accept 4-Star ticket
+    accept_ticket()
 
-    # navigate to 6-Star ticket
+    # navigate to 6-Star rotary ticket
     press_key("right")
 
-    # select and open 6-Star ticket
-    print("Opening 6-Star ticket.")
+    # select and open 6-Star rotary roulette ticket
+    print("Opening 6-Star roulette rotary ticket.")
     open_ticket()
+
+    # accept 6-Star ticket
+    accept_ticket()
 
     # navigate back to map
     print("Heading back to map.")
     press_key("escape")
     press_key("escape")
-    time.sleep(3)
+    delay(3)
 
     # place cursor on Menu to start over
-    press_key("left")  
+    press_key("left")
 
 
 def execute_bot():
@@ -179,17 +205,25 @@ def execute_bot():
     open_tickets()
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="gt7_bot by naut 2022"
     )
     parser.add_argument("--version", "-v", action="version", version="%(prog)s v0.0.2")
+    parser.add_argument("--cycles", "-c", type=int, default=True, help="Indicate how many cyles to run. Default is set to infiniti.")
+    parser.add_argument("--debug", "-db", action="store_true", help="Toggle debug mode.")
 
     args = parser.parse_args()
-
+    
     cycles = 0
-    while True:
+    time_initial = time.time()
+    while args.cycles:
         execute_bot()
         cycles += 1
-        print(f"{str(cycles)} cyles completed.")
+        time_elapsed_s = time.time() - time_initial
+        time_elapsed_m = time_elapsed_s / 60
+        print(f"{str(cycles)} cycle{'s'[:cycles^1]} completed over {time_elapsed_m} min.")
+
+        if args.cycles == cycles:
+            args.cycles = False
+            print(f"Bot operations complete.")
